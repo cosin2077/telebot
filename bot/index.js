76d8a6fa512421a51b014a5e38a2fff2da2aebfa
cosin2077 <hk3d2y@163.com>
2022-04-29 17:14:35 +0800

    feat: bot

---
+++ bot/index.js
@@ -0,0 +1,45 @@
+const HttpsProxyAgent = require('https-proxy-agent')
+const { Telegraf } = require('telegraf')
+const { v4: uuidV4 } = require('uuid')
+require('dotenv').config()
+const { startMessage, helpMessage } = require('../messages')
+const { listenEvents } = require('../listen')
+const proxyConfig = {
+  telegram: {
+    agent: new HttpsProxyAgent(process.env.HTTPS_PROXY || process.env.HTTP_PROXY || process.env.ALL_PROXY)
+  }
+}
+const bot = new Telegraf(process.env.BOT_TOKEN, proxyConfig)
+
+bot.start((ctx) => {
+  ctx.replyWithHTML(startMessage)
+})
+
+bot.command('help', async ctx => {
+  ctx.replyWithHTML(helpMessage)
+})
+
+bot.command('fact', async (ctx) => {
+  ctx.reply('Generating, Please wait !!!')
+});
+
+let listenEventMsg = ''
+listenEvents.forEach((event, index) => {
+  listenEventMsg += event + ','
+  if (index === listenEvents.length - 1) {
+    console.log(`listening: ${listenEventMsg}`)
+  }
+  bot.on(event, async ctx => {
+    console.log(ctx)
+    console.log(ctx.update.message)
+  })
+})
+bot.launch()
+.then(() => {
+  console.log('server connect bot succeed!')
+}).catch(console.log)
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+module.exports = bot
